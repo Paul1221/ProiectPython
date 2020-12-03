2da9645e5f227b282a80be586dcfb301bd3bf36e
Paul1221 <61627590+Paul1221@users.noreply.github.com>
2020-12-03 23:36:25 +0200

    Add files via upload

--- Proiect/main.py
+++ Proiect/main.py
@@ -1,5 +1,4 @@
 import random
-
 import pygame
 
 
@@ -9,6 +8,7 @@ class SnakePart:
         self.x = x
         self.y = y
         self.changes = []
+
 
 class Fruit:
     def __init__(self, x, y):
@@ -52,8 +52,8 @@ class Snake:
             x -= 10
         elif self.direction == 'right':
             x += 10
-        self.parts.append(SnakePart(x, y))
         self.draw_snake(dis)
+        self.parts.append(SnakePart(x, y))
 
     def change_direction(self, direction):
         self.direction = direction
@@ -64,8 +64,12 @@ class Game:
         super().__init__()
         self.player = Snake()
         self.gameOver = False
+        self.gameRunning = True
         self.dis = None
+        self.score = 0
+        self.highScore = 0
         self.fruit = Fruit(int(random.randrange(10, 390, 10)), int(random.randrange(10, 390, 10)))
+
         self.start_game()
 
     def start_game(self):
@@ -77,8 +81,15 @@ class Game:
         self.fruit.draw_fruit(self.dis)
         pygame.display.update()
         while not self.gameOver:
-            self.run_game()
-
+            if self.gameRunning:
+                self.run_game()
+            else:
+                self.end_screen()
+                print(self.score)
+                if self.score > self.highScore:
+                    self.highScore = self.score
+                self.reset()
+        print(self.highScore)
         pygame.quit()
         quit()
 
@@ -87,10 +98,9 @@ class Game:
         self.fruit.draw_fruit(self.dis)
         self.eat()
         self.check_end()
-        self.player.change_direction(self.player.direction)
         i = 0
         move_flag = False
-        while i < 20000:
+        while i < 15000:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.gameOver = True
@@ -100,14 +110,17 @@ class Game:
                             if self.player.direction != 'right':
                                 self.player.change_direction('left')
                                 move_flag = True
+
                         elif event.key == pygame.K_RIGHT:
                             if self.player.direction != 'left':
                                 self.player.change_direction('right')
                                 move_flag = True
+
                         elif event.key == pygame.K_UP:
                             if self.player.direction != 'down':
                                 self.player.change_direction('up')
                                 move_flag = True
+
                         elif event.key == pygame.K_DOWN:
                             if self.player.direction != 'up':
                                 self.player.change_direction('down')
@@ -115,12 +128,14 @@ class Game:
             i += 1
 
     def check_end(self):
-        if not (10 <= self.player.parts[-1].x <= 390 and 10 <= self.player.parts[-1].y <= 390):
-            self.gameOver = True
+        if not (0 <= self.player.parts[-1].x <= 390 and 0 <= self.player.parts[-1].y <= 390):
+            self.gameRunning = False
+            return
         for part in self.player.parts:
             if part.x == self.player.parts[-1].x and part.y == self.player.parts[-1].y:
                 if part != self.player.parts[-1]:
-                    self.gameOver = True
+                    self.gameRunning = True
+                    break
 
     def eat(self):
         if self.player.parts[-1].x == self.fruit.x and self.player.parts[-1].y == self.fruit.y:
@@ -135,7 +150,41 @@ class Game:
             pygame.draw.rect(self.dis, (0, 255, 0), [self.fruit.x, self.fruit.y, 10, 10])
             self.fruit.x = int(random.randrange(10, 390, 10))
             self.fruit.y = int(random.randrange(10, 390, 10))
-            self.player.draw_snake(self.dis)
             self.fruit.draw_fruit(self.dis)
+            self.score += 1
+
+    def reset(self):
+        self.player = Snake()
+        self.gameRunning = True
+        self.score = 0
+        self.dis = pygame.display.set_mode((400, 400))
+        pygame.display.update()
+        self.player.init_snake(self.dis)
+        self.fruit.draw_fruit(self.dis)
+        pygame.display.update()
+
+    def end_screen(self):
+        choosedOption = False
+        self.dis = pygame.display.set_mode((400, 400))
+        myfont = pygame.font.SysFont("monospace", 15)
+        label1 = myfont.render(f"You ate {self.score} fruits.", 1, (255, 255, 0))
+        label2 = myfont.render("Press Y to continue or N to exit.", 1, (255, 255, 0))
+        self.dis.blit(label1, (125, 100))
+        self.dis.blit(label2, (75, 200))
+        pygame.display.update()
+        while not choosedOption:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_y:
+                        choosedOption = True
+                    elif event.key == pygame.K_n:
+                        self.dis = pygame.display.set_mode((400, 400))
+                        myfont = pygame.font.SysFont("monospace", 15)
+                        label1 = myfont.render(f"Your highscore is {self.highScore}.", 1, (255, 255, 0))
+                        self.dis.blit(label1, (125, 100))
+                        pygame.display.update()
+                        pygame.time.wait(3000)
+                        self.gameOver = True
+                        choosedOption = True
 
 start = Game()
