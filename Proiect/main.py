51b4d24c46c7f07bdf6573e3b2d9c0c748a07f88
Paul1221 <61627590+Paul1221@users.noreply.github.com>
2020-12-27 20:22:21 +0200

    Add files via upload
    Added obstacles
    Made fruits not spawn over obstacles
    Added a config file(board.json)

--- Proiect/main.py
+++ Proiect/main.py
@@ -1,5 +1,8 @@
 import random
 import pygame
+import sys
+import json
+from win32api import GetSystemMetrics
 
 
 class SnakePart:
@@ -59,8 +62,19 @@ class Snake:
         self.direction = direction
 
 
+class Obstacle:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.color = (255, 255, 255)
+
+    def draw(self, dis):
+        pygame.draw.rect(dis, self.color, [self.x, self.y, 10, 10])
+        pygame.display.update()
+
+
 class Game:
-    def __init__(self):
+    def __init__(self, width, height, obstacles):
         super().__init__()
         self.player = Snake()
         self.gameOver = False
@@ -68,15 +82,21 @@ class Game:
         self.dis = None
         self.score = 0
         self.highScore = 0
-        self.fruit = Fruit(int(random.randrange(10, 390, 10)), int(random.randrange(10, 390, 10)))
-
+        self.width = width
+        self.height = height
+        self.obstacles = []
+        for ob in obstacles:
+            if 10 < ob['x'] < self.width - 10 and 10 < ob['y'] < self.height - 10:
+                self.obstacles.append(Obstacle(ob['x'], ob['y']))
+        self.fruit = Fruit(int(random.randrange(10, self.width-10, 10)), int(random.randrange(10, self.height-10, 10)))
         self.start_game()
 
     def start_game(self):
         pygame.init()
-        self.dis = pygame.display.set_mode((400, 400))
+        self.dis = pygame.display.set_mode((self.width, self.height))
         pygame.display.update()
         pygame.display.set_caption('Snake')
+        self.draw_obstacles()
         self.player.init_snake(self.dis)
         self.fruit.draw_fruit(self.dis)
         pygame.display.update()
@@ -84,12 +104,10 @@ class Game:
             if self.gameRunning:
                 self.run_game()
             else:
-                self.end_screen()
-                print(self.score)
                 if self.score > self.highScore:
                     self.highScore = self.score
+                self.end_screen()
                 self.reset()
-        print(self.highScore)
         pygame.quit()
         quit()
 
@@ -128,14 +146,18 @@ class Game:
             i += 1
 
     def check_end(self):
-        if not (0 <= self.player.parts[-1].x <= 390 and 0 <= self.player.parts[-1].y <= 390):
+        if not (0 <= self.player.parts[-1].x <= self.width-10 and 0 <= self.player.parts[-1].y <= self.height-10):
             self.gameRunning = False
             return
         for part in self.player.parts:
             if part.x == self.player.parts[-1].x and part.y == self.player.parts[-1].y:
                 if part != self.player.parts[-1]:
-                    self.gameRunning = True
+                    self.gameRunning = False
                     break
+        for ob in self.obstacles:
+            if ob.x == self.player.parts[-1].x and ob.y == self.player.parts[-1].y:
+                self.gameRunning = False
+                break
 
     def eat(self):
         if self.player.parts[-1].x == self.fruit.x and self.player.parts[-1].y == self.fruit.y:
@@ -148,8 +170,7 @@ class Game:
             elif self.player.direction == 'right':
                 self.player.parts.append(SnakePart(self.fruit.x+10, self.fruit.y))
             pygame.draw.rect(self.dis, (0, 255, 0), [self.fruit.x, self.fruit.y, 10, 10])
-            self.fruit.x = int(random.randrange(10, 390, 10))
-            self.fruit.y = int(random.randrange(10, 390, 10))
+            self.choose_fruit_xy()
             self.fruit.draw_fruit(self.dis)
             self.score += 1
 
@@ -157,34 +178,71 @@ class Game:
         self.player = Snake()
         self.gameRunning = True
         self.score = 0
-        self.dis = pygame.display.set_mode((400, 400))
+        self.dis = pygame.display.set_mode((self.width, self.height))
         pygame.display.update()
+        self.draw_obstacles()
         self.player.init_snake(self.dis)
         self.fruit.draw_fruit(self.dis)
         pygame.display.update()
 
     def end_screen(self):
-        choosedOption = False
+        choosedoption = False
         self.dis = pygame.display.set_mode((400, 400))
         myfont = pygame.font.SysFont("monospace", 15)
-        label1 = myfont.render(f"You ate {self.score} fruits.", 1, (255, 255, 0))
-        label2 = myfont.render("Press Y to continue or N to exit.", 1, (255, 255, 0))
+        label1 = myfont.render(f"You ate {self.score} fruits.", True, (255, 255, 0))
+        label2 = myfont.render("Press Y to continue or N to exit.", True, (255, 255, 0))
         self.dis.blit(label1, (125, 100))
         self.dis.blit(label2, (75, 200))
         pygame.display.update()
-        while not choosedOption:
+        while not choosedoption:
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_y:
-                        choosedOption = True
+                        choosedoption = True
                     elif event.key == pygame.K_n:
                         self.dis = pygame.display.set_mode((400, 400))
                         myfont = pygame.font.SysFont("monospace", 15)
-                        label1 = myfont.render(f"Your highscore is {self.highScore}.", 1, (255, 255, 0))
+                        label1 = myfont.render(f"Your highscore is {self.highScore}.", True, (255, 255, 0))
                         self.dis.blit(label1, (125, 100))
                         pygame.display.update()
                         pygame.time.wait(3000)
                         self.gameOver = True
-                        choosedOption = True
+                        choosedoption = True
 
-start = Game()
+    def choose_fruit_xy(self):
+        ok1 = False
+        ok2 = False
+        while not (ok1 and ok2):
+            self.fruit.x = int(random.randrange(10, self.width-10, 10))
+            self.fruit.y = int(random.randrange(10, self.height-10, 10))
+            for part in self.player.parts:
+                if self.fruit.x != part.x or self.fruit.y != part.y:
+                    ok1 = True
+                    break
+            for ob in self.obstacles:
+                if ob.x != self.fruit.x or ob.y != self.fruit.y:
+                    ok2 = True
+                    break
+
+    def draw_obstacles(self):
+        for ob in self.obstacles:
+            ob.draw(self.dis)
+
+
+with open(sys.argv[1]) as f:
+    data = json.load(f)
+
+w = data['width']
+h = data['height']
+
+if w <= 300:
+    w = 300
+if h <= 300:
+    h = 300
+if w > GetSystemMetrics(0):
+    w = GetSystemMetrics(0)
+if h > GetSystemMetrics(1):
+    h = GetSystemMetrics(1)
+
+print(w, h)
+start = Game(w, h, data['obstacles'])
